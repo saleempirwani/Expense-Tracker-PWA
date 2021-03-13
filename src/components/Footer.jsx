6a329dae37569f2e0b432ff30737151cc79d4952
Saleem Raza <salimraza3465@gmail.com>
2021-03-13 23:58:56 +0500

    footer adjustment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="mt-5 p-2 bg-dark text-white">
+    <div
+      className={
+        window.innerWidth > 600
+          ? "fixed-bottom p-2 bg-dark text-white"
+          : "mt-5 p-2 bg-dark text-white"
+      }
+    >
       Copyright &copy; {year()}, Muhammad Saleem Raza
     </div>
   );
